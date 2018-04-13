9ba730b1ffe3e071550b47ee1ae10ed1d0f1ef68
dangitstam <dangt7@uw.edu>
2018-04-13 15:49:38 -0700

    renamed folder, adding gitignore

--- machine_dictionary_rc/models/baseline.py
+++ machine_dictionary_rc/models/baseline.py
@@ -19,32 +19,34 @@ from collections import Counter
 SOS_token = 0
 EOS_token = 1
 
+hidden_size = 256
+
 class Lang:
     def __init__(self, name):
         self.name = name
-        self.word2index = {}
-        # self.word2count = {}
+        self.word2index = {"SOS": 0, "EOS": 1}
         self.index2word = {0: "SOS", 1: "EOS"}
-        self.n_words = 2  # Count SOS and EOS
+        self.nwords = 2  # Count SOS and EOS
 
     def addSentence(self, sentence):
         for word in sentence.split(' '):
             self.addWord(word)
 
     def addWord(self, word):
+        word.lower()
         if word not in self.word2index:
-            self.word2index[word] = self.n_words
-            self.index2word[self.n_words] = word
-            self.n_words += 1
+            self.word2index[word] = self.nwords
+            self.index2word[self.nwords] = word
+            self.nwords += 1
 
 lang = Lang("English")
 
-f = open("pride.txt", "r")
-lines = []
-for line in f:
-    lang.addSentence(line)
-    lines.append(line)
+with open("pride.txt") as f:
+    text = f.read()
 
+sentences = re.split(r' *[\.\?!][\'"\)\]]* *', text)
+for sentence in sentences:
+    lang.addSentence(sentence)
 
 class EncoderRNN(nn.Module):
     def __init__(self, input_size, hidden_size):
@@ -54,22 +56,18 @@ class EncoderRNN(nn.Module):
         self.i2h = nn.Linear(hidden_size, hidden_size)
         self.i2o = nn.Linear(hidden_size, input_size)
         self.o2o = nn.Linear(hidden_size, input_size)
-
+        self.gru = nn.GRU(hidden_size, hidden_size)
         self.dropout = nn.Dropout(0.1)
-        self.softmax = nn.LogSoftmax(dim=1)
 
     def forward(self, word, hidden):
         embedded = self.embedding(word).view(1, 1, -1)
-        # print("embedded", embedded.size())
-        # print("hidden", hidden.size())
-        input_combined = torch.cat((hidden, embedded), 1)
-        # print(input_combined.size())
-        new_hidden = self.i2h(embedded + hidden)
+        output, hidden = self.gru(embedded, hidden)
+        newHidden = self.i2h(embedded + hidden)
         output = self.i2o(embedded + hidden)
         output = self.dropout(output)
-        # output = self.o2o(new_hidden + embedded)
-        # output = self.softmax(output)
-        return output, new_hidden
+
+        hidden = newHidden
+        return output, hidden
 
     def initHidden(self):
         return Variable(torch.zeros(1, 1, self.hidden_size))
@@ -79,6 +77,7 @@ def indexesFromSentence(lang, sentence):
 
 def variableFromSentence(lang, sentence):
     indexes = indexesFromSentence(lang, sentence)
+    indexes.append(EOS_token)
     result = Variable(torch.LongTensor(indexes).view(-1, 1))
     return result
 
@@ -87,73 +86,50 @@ def firstWord(lang, word):
     result = Variable(torch.LongTensor(indexes).view(-1, 1))
     return result
 
-def train(input_variable, encoder, criterion):
+def train(input_variable, encoder, encoder_optimizer, criterion):
     # initialize the hidden states of the encoder
     hidden = encoder.initHidden()
-    encoder.zero_grad()
+    encoder_optimizer.zero_grad()
+    input_len = input_variable.size()[0]
     loss = Variable(torch.zeros(1))
-    for i in range(len(input_variable)):
+    for i in range(input_len):
         output, hidden = encoder(
             input_variable[i], hidden)
-        # print("HEY", output[0].size())
-        if i != len(input_variable) - 1:
+        if i != input_len - 1:
             loss += criterion(output[0], input_variable[i + 1])
 
     if loss.data[0] != 0:
         loss.backward()
+    encoder_optimizer.step()
 
-# just figuring out the size of the hidden state and stuff
-hidden_size = 128
-# initializing the encoder and reversecoder
-encoder = EncoderRNN(lang.n_words, hidden_size)
+# initializing the encoder
+encoder = EncoderRNN(lang.nwords, hidden_size)
 criterion = nn.NLLLoss()
 
-learning_rate = 0.0005
-# the actual training part happens here (run for some epochs over the training data)
+encoder_optimizer = optim.SGD(encoder.parameters(), 0.001)
+
+# training section begins
 print("Training here...")
-for i in range(200):
+for i in range(200, 600):
     print(i)
-    input_variable = variableFromSentence(lang, lines[i])
-    train(input_variable, encoder, criterion)
+    input_variable = variableFromSentence(lang, sentences[i])
+    train(input_variable, encoder, encoder_optimizer, criterion)
 
-def sample(input_variable, encoder):
-    encoder_hidden = encoder.initHidden()
+# sampling the first 20 words from the seed (can be modified later)
+def sample(starter, encoder):
+    ret = []
+    output = firstWord(lang, starter)
+    hidden = encoder.initHidden()
+    ret.append(starter)
+    for i in range(20):
+        output, hidden = encoder(output, hidden)
+        topv, topi = output.data.topk(1)
+        topi = topi[0][0][0]
+        word = lang.index2word[topi]
+        ret.append(word)
+        output = firstWord(lang, word)
+    return ret
 
-    for i in range(100):
-        hidden = encoder.initHidden()
-        # encoder.zero_grad()
-        loss = Variable(torch.zeros(1))
-        for i in range(len(input_variable)):
-            output, hidden = encoder(
-                input_variable[i], hidden)
-            print("OUTPUT", output)
-            best_index = 0
-            best_value = -1 * float("inf")
-            for i in range(len(output.data)):
-                if output.data[i] > best_value:
-                    best_index = i
-                    best_value = output.data[i]
-            print("BEST INDEX", best_index)
-            # topv, topi = output.data.topk(1)
-            # # print("topi", topi)
-            # topi = topi[0][0]
-            # print(topi[0])
-            # word = lang.index2word[topi[0]]
-
-        # encoder_output, encoder_hidden = encoder(encoder_output, encoder_hidden)
-        # print("OUTPUT", encoder_output)
-        # input = encoder_output
-        # print(encoder_output[0].size(), "SIZE")
-        # print("HIHIHI", encoder_output[0].sum())
-        # topv, topi = encoder_output.data.topk(1)
-        # print("topi", topi)
-        # # topi = topi[0][0]
-        #
-        # word = lang.index2word[topi]
-        # print("WORD", word)
-
-w = "The"
+w = "the"
 wformat = firstWord(lang, w)
-# inputt = Variable(torch.LongTensor([[23]]))
-# print(wformat.size(), "HEEYYY")
-sample(wformat, encoder)
+sample(w, encoder)
