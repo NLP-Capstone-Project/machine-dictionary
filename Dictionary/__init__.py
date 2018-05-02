061e4e59bcafa450a60caba6e7edd097008fed51
dangitstam <dangt7@uw.edu>
2018-05-02 15:22:39 -0700

    final removal of fsm

--- Dictionary/__init__.py
+++ Dictionary/__init__.py
@@ -1,5 +1,4 @@
 from .data import Corpus, UMLSCorpus
 from .utils import word_vector_from_seq, extract_tokens_from_json
-from .fsm import FSM
 from .umls import UMLS
 from .extractor import Extractor
