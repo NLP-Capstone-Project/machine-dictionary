891b059312d54dbd83a290f26194dfea02be3c44
Karishma Mandyam <karishma.mandyam@gmail.com>
2018-05-21 16:09:58 -0700

    adds in shell for generating training example script, cleans up word vector ranking script

--- generate_training_examples.py
+++ generate_training_examples.py
@@ -8,7 +8,6 @@ import sys
 from tqdm import tqdm
 
 sys.path.append(os.path.join(os.path.dirname(__file__)))
-from Dictionary import load_embeddings, Extractor
 
 
 def main():
@@ -18,16 +17,8 @@ def main():
                         help="Path to directory of directories")
     parser.add_argument("--save-dir", type=str,
                         help="Path to save relevant examples.")
-    parser.add_argument("--vocabulary-path", type=str,
-                        help="Path to the vocabulary")
-    parser.add_argument("--glove-path", type=str,
-                        help="Path to the glove vectors")
 
     args = parser.parse_args()
-
-    # Reading in the vocabulary and glove vectors
-    print("Parsing existing vocabulary")
-    embedding_matrix, vocabulary = load_embeddings(args.glove_path, args.vocabulary_path)
 
     print("Creating saved directory")
     if not os.path.exists(args.save_dir):
@@ -35,7 +26,6 @@ def main():
 
     print("Reading files and Extracting")
     all_paper_directories = os.listdir(args.documents_path)
-    extractor = Extractor(embedding_matrix, vocabulary)
     for directory in tqdm(all_paper_directories):
         directory_path = os.path.join(args.documents_path, directory)
         files = os.listdir(directory_path)
@@ -46,21 +36,16 @@ def main():
 
         for file in files:
             file_path = os.path.join(directory_path, file)
-            prefix_document = json.load(open(file_path, 'r'))
-            alias_sentences = extractor.obtain_sentences_with_alias(prefix_document["aliases"],
-                                                                    prefix_document["sentences"])
-            chosen_sentences = extractor.rank_sentences_word_vectors(alias_sentences,
-                                                                     prefix_document["definition"])
-            target_vector = [0] * len(prefix_document["sentences"])
-            for (score, sentence, index) in chosen_sentences:
-                target_vector[index] = 1
+            ranked_document = json.load(open(file_path, 'r'))
+
+            entity = ""
+
             output = collections.OrderedDict(
-                [("title", prefix_document["title"]),
-                 ("definition", prefix_document["definition"]),
-                 ("aliases", prefix_document["aliases"]),
-                 ("sentences", prefix_document["sentences"]),
-                 ("target", target_vector),
-                 ("chosen_sentences_and_ranks", chosen_sentences)]
+                [("title", ranked_document["title"]),
+                 ("entity", entity),
+                 ("definition", ranked_document["definition"]),
+                 ("sentences", ranked_document["sentences"]),
+                 ("target", ranked_document["target"])]
             )
 
             file_save_path = os.path.join(document_save_dir, file)
